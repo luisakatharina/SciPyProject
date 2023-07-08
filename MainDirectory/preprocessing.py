8ec9f141a21fe096bbe3e34acb85ed8e0de3e0d0
luisakatharina <labels@uni-osnabrueck.de>
2023-07-08 07:41:21 +0200

    added: expand_contractions and some fixes

--- MainDirectory/preprocessing.py
+++ MainDirectory/preprocessing.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 import json
 import nltk
+import re
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-
-# Downloading necessary NLTK resources
 nltk.download('stopwords')
 
 
@@ -23,20 +22,24 @@ with open("C:/Users/Luisa/Documents/Uni/SoSe 2023/SciPy/Project/Sarcasm_Headline
 df = pd.DataFrame(data)
 
 
-
 # Actual Text Preprocessing
-# removing stopwords
+# Removing stopwords
 stopwords = set(stopwords.words('english'))
 
 def preprocess_text(text):
     '''
     function to perform the text preprocessing steps: converting to lowercase, 
-    removing punctuation and special characters, tokenization, and removing stopwords
+    removing punctuation, special characters, numbers, tokenization, 
+    expanding contractions and removing stopwords
     '''
-    # Convert to lowercase
+    # To lowercase
     text = text.lower()
-    # Remove punctuation and special characters
+    # Remove punctuation, special characters
     text = text.replace('[^\w\s]', '')
+    # Expand contractions 
+    text = expand_contractions(text)
+    # Remove numbers 
+    text = re.sub(r'\d+', '', text)
     # Tokenization
     tokens = nltk.word_tokenize(text)
     # Remove stopwords
@@ -47,29 +50,49 @@ def preprocess_text(text):
     return processed_text
 
 
+def expand_contractions(text):
+    '''
+    Function to expand contractions in the text
+    '''
+    contractions = {
+        "can't": "cannot",
+        "won't": "will not",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "haven't": "have not",
+        "hasn't": "has not",
+        "wouldn't": "would not",
+        "shouldn't": "should not",
+        "isn't": "is not"
+    }
+    pattern = re.compile(r'\b(' + '|'.join(contractions.keys()) + r')\b')
+    expanded_text = pattern.sub(lambda match: contractions[match.group(0)], text)
+    return expanded_text
+
+
+
 # Applying to 'headline' column
-# Processed headlines stored in new column
+# Processed headlines stored in new column (to preserve original text data)
 df['processed_headline'] = df['headline'].apply(preprocess_text)
 
 
 # LabelEncoder from sklearn.preprocessing module to encode target variable 'is_sarcastic'
-# from categorical values to numeric values
-# Then stored in 'is_sarcastic_encoded'
+# From categorical values to numeric values, then stored in 'is_sarcastic_encoded'
 label_encoder = LabelEncoder()
 df['is_sarcastic_encoded'] = label_encoder.fit_transform(df['is_sarcastic'])
 
 
 # TF-IDF feature extraction --> Feature matrix X
-# converts textual data in 'processed_headline' column into numerical feature matrix X
+# Convert textual data in 'processed_headline' column into numerical feature matrix X
 tfidf_vectorizer = TfidfVectorizer()
 X = tfidf_vectorizer.fit_transform(df['processed_headline'])
-y = df['is_sarcastic_encoded']    #value corresponds to the encoded class of sarcasm (0 or 1)
+y = df['is_sarcastic_encoded']    # each value 0 or 1 (class of sarcasm)
 
 
 # Splitting data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=32)
 
 
 
-# testing
+# testing (for me/us right now)
 print(df['processed_headline'].head())
